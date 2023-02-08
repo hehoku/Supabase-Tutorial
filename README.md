43d59b41ec511b405ac6ef02db419cc56c8c4b68
hehoku <hehoku@outlook.com>
2023-02-08 23:52:24 +0800

    feat: Add edit button, navigate to item

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+Refer: https://github.com/iamshaunjp/Supabase-Tutorial-for-Beginners
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,18 @@
+import { Edit } from "react-feather";
+import { Link } from "react-router-dom";
+import "./work.css";
+
 export default function Work({ work }) {
   return (
     <div className="work">
       <a href={work.url}>
         <p>{work.title}</p>
       </a>
+      <button>
+        <Link to={"/" + work.id}>
+          <Edit />
+        </Link>
+      </button>
     </div>
   );
 }
