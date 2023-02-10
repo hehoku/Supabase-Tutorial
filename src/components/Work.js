9565e45f432016c232e24a1505ad4627c64a680d
hehoku <hehoku@outlook.com>
2023-02-10 23:39:22 +0800

    feat: support delete work item

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,27 @@
-import { Edit } from "react-feather";
+import { Edit, Trash2 } from "react-feather";
 import { Link } from "react-router-dom";
+
+import supabase from "../config/supabaseClient";
+import "../config/supabaseClient";
 import "./work.css";
 
 export default function Work({ work }) {
+  let handleDelete = async () => {
+    let { data, error } = await supabase
+      .from("works")
+      .delete()
+      .eq("id", work.id)
+      .select();
+
+    if (data) {
+      console.log(data);
+    }
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="work">
       <a href={work.url}>
@@ -12,6 +31,10 @@ export default function Work({ work }) {
         <Link to={"/" + work.id}>
           <Edit />
         </Link>
+      </button>
+
+      <button onClick={handleDelete} className="delete">
+        <Trash2 />
       </button>
     </div>
   );
