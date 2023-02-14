a529b60d22f8ab7645d2faa6eae241c70d0d069a
hehoku <hehoku@outlook.com>
2023-02-14 22:29:14 +0800

    feat: sort by created time or title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import "./home.css";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [works, setWorks] = useState(null);
-
+  const [order, setOrder] = useState("created_at");
   const handleDelete = (id) => {
     setWorks((prevWorks) => {
       prevWorks.filter((work) => work.id !== id);
@@ -18,7 +18,7 @@ const Home = () => {
       let { data, error } = await supabase
         .from("works")
         .select()
-        .order("id", { ascending: true });
+        .order(order, { ascending: false });
 
       if (error) {
         setFetchError("Could not fetch the works");
@@ -33,11 +33,24 @@ const Home = () => {
     };
 
     fetchWorks();
-  }, []);
+  }, [order]);
 
   return (
     <div className="page home">
       <h2>Home</h2>
+      <button
+        className={order === "created_at" ? "active sortBtn" : "sortBtn"}
+        onClick={() => setOrder("created_at")}
+      >
+        created_at
+      </button>
+      <button
+        className={order === "title" ? "active sortBtn" : "sortBtn"}
+        onClick={() => setOrder("title")}
+      >
+        title
+      </button>
+
       {fetchError && <p>{fetchError}</p>}
       {works && (
         <div className="worksContainer">
